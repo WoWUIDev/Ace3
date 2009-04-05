d985ce074582f0a43218d4613e25a5af94fba64d
Hendrik Leppkes <h.leppkes@gmail.com>
2009-04-05 09:02:35 +0000

    Add a usage example to AceBucket

--- AceBucket-3.0/AceBucket-3.0.lua
+++ AceBucket-3.0/AceBucket-3.0.lua
@@ -2,22 +2,35 @@
 -- your addon only needs to know about the full burst.
 -- 
 -- This Bucket implementation works as follows:\\
--- : Initially, no schedule is running, and its waiting for the first event to happen.\\
--- : The first event will start the bucket, and get the scheduler running, which will collect all
+--   Initially, no schedule is running, and its waiting for the first event to happen.\\
+--   The first event will start the bucket, and get the scheduler running, which will collect all
 --   events in the given interval. When that interval is reached, the bucket is pushed to the 
 --   callback and a new schedule is started. When a bucket is empty after its interval, the scheduler is 
---   stopped, and the bucket is only listening for the next event to happen, basicly back in initial state.
+--   stopped, and the bucket is only listening for the next event to happen, basically back in its initial state.
 -- 
 -- In addition, the buckets collect information about the "arg1" argument of the events that fire, and pass those as a 
 -- table to your callback. This functionality was mostly designed for the UNIT_* events.\\
 -- The table will have the different values of "arg1" as keys, and the number of occurances as their value, e.g.\\
--- : { ["player"] = 2, ["target"] = 1, ["party1"] = 1 }
+--   { ["player"] = 2, ["target"] = 1, ["party1"] = 1 }
 --
 -- **AceBucket-3.0** can be embeded into your addon, either explicitly by calling AceBucket:Embed(MyAddon) or by 
 -- specifying it as an embeded library in your AceAddon. All functions will be available on your addon object
 -- and can be accessed directly, without having to explicitly call AceBucket itself.\\
 -- It is recommended to embed AceBucket, otherwise you'll have to specify a custom `self` on all calls you
 -- make into AceBucket.
+-- @usage
+-- local MyAddon = LibStub("AceAddon-3.0"):NewAddon("BucketExample", "AceBucket-3.0")
+-- 
+-- function MyAddon:OnEnable()
+--   -- Register a bucket that listens to all the HP related events, and fires once per second
+--   self.hpBucket = self:RegisterBucketEvent({"UNIT_HEALTH", "UNIT_MAXHEALTH"}, 1, "UpdateHealth")
+-- end
+--
+-- function MyAddon:UpdateHealth(units)
+--   if units.player then
+--     print("Your HP changed!")
+--   end
+-- end
 -- @class file
 -- @name AceBucket-3.0.lua
 -- @release $Id$
