2ff2f94326c788c6d06571d7b266ef4f5f3215bc
Hendrik Leppkes <h.leppkes@gmail.com>
2010-05-13 09:40:08 +0000

    AceGUI-3.0: Cleanup and refactoring of the Label widget. It'll properly reset its font now (Ticket #108)

--- AceGUI-3.0/widgets/AceGUIWidget-Label.lua
+++ AceGUI-3.0/widgets/AceGUIWidget-Label.lua
@@ -1,4 +1,6 @@
-local AceGUI = LibStub("AceGUI-3.0")
+local Type, Version = "Label", 20
+local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
+if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
 
 -- Lua APIs
 local max, select = math.max, select
@@ -10,85 +12,87 @@ local CreateFrame, UIParent = CreateFrame, UIParent
 -- List them here for Mikk's FindGlobals script
 -- GLOBALS: GameFontHighlightSmall
 
---------------------------
--- Label 	 			--
---------------------------
-do
-	local Type = "Label"
-	local Version = 12
+--[[-----------------------------------------------------------------------------
+Scripts
+-------------------------------------------------------------------------------]]
+
+local function UpdateImageAnchor(self)
+	local frame = self.frame
+	local width = frame.width or frame:GetWidth() or 0
+	local image = self.image
+	local label = self.label
+	local height
+
+	label:ClearAllPoints()
+	image:ClearAllPoints()
+
+	if self.imageshown then
+		local imagewidth = image:GetWidth()
+		if (width - imagewidth) < 200 or (label:GetText() or "") == "" then
+			-- image goes on top centered when less than 200 width for the text, or if there is no text
+			image:SetPoint("TOP", frame, "TOP", 0, 0)
+			label:SetPoint("TOP", image, "BOTTOM", 0, 0)
+			label:SetPoint("LEFT", frame, "LEFT", 0, 0)
+			label:SetPoint("RIGHT", frame, "RIGHT", 0, 0)
+			height = image:GetHeight() + label:GetHeight()
+		else
+			-- image on the left
+			image:SetPoint("TOPLEFT", frame, "TOPLEFT", 0, 0)
+			label:SetPoint("TOPLEFT", image, "TOPRIGHT", 4, 0)
+			label:SetPoint("TOPRIGHT", frame, "TOPRIGHT", 0, 0)
+			height = max(image:GetHeight(), label:GetHeight())
+		end
+	else
+		-- no image shown
+		label:SetPoint("TOPLEFT", frame, "TOPLEFT", 0, 0)
+		label:SetPoint("TOPRIGHT", frame, "TOPRIGHT", 0, 0)
+		height = label:GetHeight()
+	end
 	
-	local function OnAcquire(self)
+	self.resizing = true
+	frame:SetHeight(height)
+	frame.height = height
+	self.resizing = nil
+end
+
+--[[-----------------------------------------------------------------------------
+Methods
+-------------------------------------------------------------------------------]]
+local methods = {
+	["OnAcquire"] = function(self)
 		self:SetHeight(18)
 		self:SetWidth(200)
 		self:SetText("")
 		self:SetImage(nil)
 		self:SetImageSize(16, 16)
 		self:SetColor()
-		self:SetFontObject()
-	end
-	
-	local function OnRelease(self)
+		self.label:SetFontObject(nil)
+		self.label:SetFont(GameFontHighlightSmall:GetFont())
+	end,
+
+	["OnRelease"] = function(self)
 		self.frame:ClearAllPoints()
 		self.frame:Hide()
-	end
-	
-	local function UpdateImageAnchor(self)
-		local width = self.frame.width or self.frame:GetWidth() or 0
-		local image = self.image
-		local label = self.label
-		local frame = self.frame
-		local height
-		
-		label:ClearAllPoints()
-		image:ClearAllPoints()
-		
-		if self.imageshown then
-			local imagewidth = image:GetWidth()
-			if (width - imagewidth) < 200 or (label:GetText() or "") == "" then
-				--image goes on top centered when less than 200 width for the text, or if there is no text
-				image:SetPoint("TOP",frame,"TOP",0,0)
-				label:SetPoint("TOP",image,"BOTTOM",0,0)
-				label:SetPoint("LEFT",frame,"LEFT",0,0)
-				label:SetWidth(width)
-				height = image:GetHeight() + label:GetHeight()
-			else
-				--image on the left
-				image:SetPoint("TOPLEFT",frame,"TOPLEFT",0,0)
-				label:SetPoint("TOPLEFT",image,"TOPRIGHT",4,0)
-				label:SetWidth(width - imagewidth)
-				height = max(image:GetHeight(), label:GetHeight())
-			end
-		else
-			--no image shown
-			label:SetPoint("TOPLEFT",frame,"TOPLEFT",0,0)
-			label:SetWidth(width)
-			height = self.label:GetHeight()
-		end
-		
-		self.resizing = true
-		self.frame:SetHeight(height)
-		self.frame.height = height
-		self.resizing = nil
-	end
-	
-	local function SetText(self, text)
+	end,
+
+	["SetText"] = function(self, text)
 		self.label:SetText(text or "")
 		UpdateImageAnchor(self)
-	end
-	
-	local function SetColor(self, r, g, b)
+	end,
+
+	["SetColor"] = function(self, r, g, b)
 		if not (r and g and b) then
 			r, g, b = 1, 1, 1
 		end
 		self.label:SetVertexColor(r, g, b)
-	end
-	
-	local function OnWidthSet(self, width)
+	end,
+
+	["OnWidthSet"] = function(self, width)
 		if self.resizing then return end
 		UpdateImageAnchor(self)
-	end
-	
-	local function SetImage(self, path, ...)
+	end,
+
+	["SetImage"] = function(self, path, ...)
 		local image = self.image
 		image:SetTexture(path)
 		
@@ -104,55 +108,51 @@ do
 			self.imageshown = nil
 		end
 		UpdateImageAnchor(self)
-	end
-	
-	local function SetFont(self, font, height, flags)
+	end,
+
+	["SetFont"] = function(self, font, height, flags)
 		self.label:SetFont(font, height, flags)
-	end
-	
-	local function SetFontObject(self, font)
+	end,
+
+	["SetFontObject"] = function(self, font)
 		self.label:SetFontObject(font or GameFontHighlightSmall)
-	end
-	
-	local function SetImageSize(self, width, height)
+	end,
+
+	["SetImageSize"] = function(self, width, height)
 		self.image:SetWidth(width)
 		self.image:SetHeight(height)
 		UpdateImageAnchor(self)
-	end
+	end,
+}
+	--[[-----------------------------------------------------------------------------
+Constructor
+-------------------------------------------------------------------------------]]
+local function Constructor()
+	local frame = CreateFrame("Frame", nil, UIParent)
+	frame:Hide()
 
-	local function Constructor()
-		local frame = CreateFrame("Frame",nil,UIParent)
-		local self = {}
-		self.type = Type
-		
-		self.OnRelease = OnRelease
-		self.OnAcquire = OnAcquire
-		self.SetText = SetText
-		self.SetColor = SetColor
-		self.frame = frame
-		self.OnWidthSet = OnWidthSet
-		self.SetImage = SetImage
-		self.SetImageSize = SetImageSize
-		self.SetFont = SetFont
-		self.SetFontObject = SetFontObject
-		frame.obj = self
-		
-		frame:SetHeight(18)
-		frame:SetWidth(200)
-		local label = frame:CreateFontString(nil,"BACKGROUND","GameFontHighlightSmall")
-		label:SetPoint("TOPLEFT",frame,"TOPLEFT",0,0)
-		label:SetWidth(200)
-		label:SetJustifyH("LEFT")
-		label:SetJustifyV("TOP")
-		self.label = label
-		
-		local image = frame:CreateTexture(nil,"BACKGROUND")
-		self.image = image
-		
-		AceGUI:RegisterAsWidget(self)
-		return self
+	local label = frame:CreateFontString(nil, "BACKGROUND", "GameFontHighlightSmall")
+	label:SetPoint("TOPLEFT", frame, "TOPLEFT", 0, 0)
+	label:SetPoint("TOPRIGHT", frame, "TOPRIGHT", 0, 0)
+	label:SetJustifyH("LEFT")
+	label:SetJustifyV("TOP")
+
+	local image = frame:CreateTexture(nil, "BACKGROUND")
+
+	-- create widget
+	local widget = {
+		label = label,
+		image = image,
+		frame = frame,
+		type  = Type
+	}
+	for method, func in pairs(methods) do
+		widget[method] = func
 	end
-	
-	AceGUI:RegisterWidgetType(Type,Constructor,Version)
+	frame.obj, label.obj, image.obj = widget, widget, widget
+
+	AceGUI:RegisterAsWidget(widget)
+	return widget
 end
 
+AceGUI:RegisterWidgetType(Type, Constructor, Version)
