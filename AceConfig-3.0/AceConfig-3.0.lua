22b88265c6c6718133a87a7a8bddf6bc83c5f217
shefki <shefki@localhost>
2010-10-07 02:11:48 +0000

    Add link to documentation of the contents of the options table.

--- AceConfig-3.0/AceConfig-3.0.lua
+++ AceConfig-3.0/AceConfig-3.0.lua
@@ -36,7 +36,7 @@ local pcall, error, type, pairs = pcall, error, type, pairs
 -- You can supply a slash command (or a table of slash commands) to register with AceConfigCmd directly.
 -- @paramsig appName, options [, slashcmd]
 -- @param appName The application name for the config table.
--- @param options The option table (or a function to generate one on demand)
+-- @param options The option table (or a function to generate one on demand).  http://www.wowace.com/addons/ace3/pages/ace-config-3-0-options-tables/
 -- @param slashcmd A slash command to register for the option table, or a table of slash commands.
 -- @usage
 -- local AceConfig = LibStub("AceConfig-3.0")
