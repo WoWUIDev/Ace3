161bb4e99d4703a044fa5b48993bd172e8a1ed0e
mikk <mikk@localhost>
2011-01-26 02:13:15 +0000

    - Tests for changes in previous commit

--- tests/AceLocale-3.0.lua
+++ tests/AceLocale-3.0.lua
@@ -4,27 +4,55 @@ dofile("../AceLocale-3.0/AceLocale-3.0.lua")
 
 local AceLocale = LibStub("AceLocale-3.0")
 
-local locale_1 = AceLocale:NewLocale("test", "enUS")
-locale_1["a"] = "A"
-locale_1["c"] = "C"
+local loc = AceLocale:NewLocale("test", "enUS")
+loc["a"] = "A"
+loc["c"] = "C"
 
-local locale_2 = AceLocale:NewLocale("test", "deDE", true)
-locale_2["a"] = "aa"
-locale_2["b"] = "bb"
-locale_2["c"] = "cc"
+local loc = AceLocale:NewLocale("test", "deDE", true)
+loc["a"] = "aa"
+loc["b"] = "bb"
+loc["c"] = "cc"
 
 local locale_3 = AceLocale:NewLocale("test", "frFR")
+assert(loc == nil)
 
-local locale_4 = AceLocale:GetLocale("test")
 
---[[ no longer true with proxy metatables /mikk   assert(locale_2 == locale_1) ]]
-assert(locale_3 == nil)
---[[ no longer true with proxy metatables /mikk   assert(locale_4 == locale_1) ]]
+local test = AceLocale:GetLocale("test")
 
-print(locale_4["a"], locale_4["b"], locale_4["c"])
-assert(locale_4["a"] == "A")
-assert(locale_4["b"] == "bb")
-assert(locale_4["c"] == "C")
+assert(test["a"] == "A")
+assert(test["b"] == "bb")
+assert(test["c"] == "C")
+
+-- Test requesting an unknown string
+local oldgeterrorhandler = geterrorhandler
+local errors=0
+ _G.geterrorhandler = function() return function() errors=errors+1 end end
+assert(test["thisdoesntexist"]=="thisdoesntexist")
+assert(errors==1)
+_G.geterrorhandler=oldgeterrorhandler
+
+------------------------------------------------
+-- Test the silent flag working 
+
+AceLocale:NewLocale("test2", "enUS", true, true) -- silent flag set on first locale to be registered
+AceLocale:NewLocale("test2", "deDE")
+AceLocale:NewLocale("test2", "frFR")
+
+local test2=AceLocale:GetLocale("test2")
+assert(test2["thisdoesntexist"]=="thisdoesntexist")
+
+
+------------------------------------------------
+-- Test the silent flag working even if the default locale is registered second
+
+AceLocale:NewLocale("test3", "deDE", false, true)	-- silent flag set on first locale to be registered
+AceLocale:NewLocale("test3", "enUS", true)
+AceLocale:NewLocale("test3", "frFR")
+
+local test3=AceLocale:GetLocale("test2")
+assert(test3["thisdoesntexist"]=="thisdoesntexist")
+
+
 
 ------------------------------------------------
 print "OK"
