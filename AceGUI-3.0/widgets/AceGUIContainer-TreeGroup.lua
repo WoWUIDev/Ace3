95f35e14b80ab70e3d7b56cc37f5e4816364fbbb
Hendrik Leppkes <h.leppkes@gmail.com>
2013-09-13 14:46:46 +0000

    AceGUI-3.0: TreeGroup: Remove left over debug code

--- AceGUI-3.0/widgets/AceGUIContainer-TreeGroup.lua
+++ AceGUI-3.0/widgets/AceGUIContainer-TreeGroup.lua
@@ -2,7 +2,7 @@
 TreeGroup Container
 Container that uses a tree control to switch between groups.
 -------------------------------------------------------------------------------]]
-local Type, Version = "TreeGroup", 35
+local Type, Version = "TreeGroup", 36
 local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
 if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
 
@@ -466,7 +466,6 @@ local methods = {
 		end
 
 		local buttonnum = 1
-		print(first,last,numlines)
 		for i = first, last do
 			local line = lines[i]
 			local button = buttons[buttonnum]
