c4d25714a71376589b7dbf884b1d520e2a3a3835
Hendrik Leppkes <h.leppkes@gmail.com>
2020-04-14 22:21:22 +0000

    AceConfigDialog-3.0: Prevent a dialog refresh when a multiselect control is closed during release (Fixes #539)

--- AceConfig-3.0/AceConfigDialog-3.0/AceConfigDialog-3.0.lua
+++ AceConfig-3.0/AceConfigDialog-3.0/AceConfigDialog-3.0.lua
@@ -7,7 +7,7 @@ local LibStub = LibStub
 local gui = LibStub("AceGUI-3.0")
 local reg = LibStub("AceConfigRegistry-3.0")
 
-local MAJOR, MINOR = "AceConfigDialog-3.0", 78
+local MAJOR, MINOR = "AceConfigDialog-3.0", 79
 local AceConfigDialog, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 if not AceConfigDialog then return end
@@ -911,7 +911,7 @@ end
 
 local function MultiControlOnClosed(widget, event, ...)
 	local user = widget:GetUserDataTable()
-	if user.valuechanged then
+	if user.valuechanged and not widget:IsReleasing() then
 		local iscustom = user.rootframe:GetUserData("iscustom")
 		local basepath = user.rootframe:GetUserData("basepath") or emptyTbl
 		if iscustom then
