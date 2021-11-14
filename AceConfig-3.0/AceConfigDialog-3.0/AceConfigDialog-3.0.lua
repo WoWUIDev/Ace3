1a8144519b6e9a6c35db9cc905c24579ae722eb4
Hendrik Leppkes <h.leppkes@gmail.com>
2021-11-14 09:14:15 +0000

    AceConfigDialog-3.0: Fix width number multiplier on multiselects (Issue #584)

--- AceConfig-3.0/AceConfigDialog-3.0/AceConfigDialog-3.0.lua
+++ AceConfig-3.0/AceConfigDialog-3.0/AceConfigDialog-3.0.lua
@@ -7,7 +7,7 @@ local LibStub = LibStub
 local gui = LibStub("AceGUI-3.0")
 local reg = LibStub("AceConfigRegistry-3.0")
 
-local MAJOR, MINOR = "AceConfigDialog-3.0", 81
+local MAJOR, MINOR = "AceConfigDialog-3.0", 82
 local AceConfigDialog, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 
 if not AceConfigDialog then return end
@@ -1368,7 +1368,7 @@ local function FeedOptions(appName, options,container,rootframe,path,group,inlin
 							elseif width == "half" then
 								check:SetWidth(width_multiplier / 2)
 							elseif (type(width) == "number") then
-								control:SetWidth(width_multiplier * width)
+								check:SetWidth(width_multiplier * width)
 							elseif width == "full" then
 								check.width = "fill"
 							else
