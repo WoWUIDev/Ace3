417800f409b1b8b799390d337f40e29efbec7757
Hendrik Leppkes <h.leppkes@gmail.com>
2022-01-10 16:25:37 +0000

    AceGUI-3.0: Dropdown: Fix double click sound on toggling the dropdown (Issue #575)

--- AceGUI-3.0/widgets/AceGUIWidget-DropDown.lua
+++ AceGUI-3.0/widgets/AceGUIWidget-DropDown.lua
@@ -351,7 +351,7 @@ end
 
 do
 	local widgetType = "Dropdown"
-	local widgetVersion = 35
+	local widgetVersion = 36
 
 	--[[ Static data ]]--
 
@@ -376,7 +376,6 @@ do
 
 	local function Dropdown_TogglePullout(this)
 		local self = this.obj
-		PlaySound(856) -- SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON
 		if self.open then
 			self.open = nil
 			self.pullout:Close()
