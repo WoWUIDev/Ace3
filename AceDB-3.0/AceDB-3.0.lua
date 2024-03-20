9d5c1375a4026bb1079fa68a5da654ec3af3a8af
Hendrik Leppkes <h.leppkes@gmail.com>
2024-03-20 23:35:31 +0100

    AceDB-3.0: Sync type checks for New and ResetDB defaultProfile

--- AceDB-3.0/AceDB-3.0.lua
+++ AceDB-3.0/AceDB-3.0.lua
@@ -41,7 +41,7 @@
 -- @class file
 -- @name AceDB-3.0.lua
 -- @release $Id$
-local ACEDB_MAJOR, ACEDB_MINOR = "AceDB-3.0", 28
+local ACEDB_MAJOR, ACEDB_MINOR = "AceDB-3.0", 29
 local AceDB = LibStub:NewLibrary(ACEDB_MAJOR, ACEDB_MINOR)
 
 if not AceDB then return end -- No upgrade needed
@@ -606,8 +606,8 @@ end
 -- profile.
 -- @param defaultProfile The profile name to use as the default
 function DBObjectLib:ResetDB(defaultProfile)
-	if defaultProfile and type(defaultProfile) ~= "string" then
-		error(("Usage: AceDBObject:ResetDB(defaultProfile): 'defaultProfile' - string or nil expected, got %q."):format(type(defaultProfile)), 2)
+	if defaultProfile and type(defaultProfile) ~= "string" and defaultProfile ~= true then
+		error(("Usage: AceDBObject:ResetDB(defaultProfile): 'defaultProfile' - string or true expected, got %q."):format(type(defaultProfile)), 2)
 	end
 
 	local sv = self.sv
