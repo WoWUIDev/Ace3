6531a7f377b140ecccae494044a63068dc8e20bb
funkydude <funkydude@users.noreply.github.com>
2026-09-06 22:47:39 +0000

    AceGUI-3.0: Forgot to bump versions.

--- AceGUI-3.0/widgets/AceGUIWidget-Icon.lua
+++ AceGUI-3.0/widgets/AceGUIWidget-Icon.lua
@@ -1,7 +1,7 @@
 --[[-----------------------------------------------------------------------------
 Icon Widget
 -------------------------------------------------------------------------------]]
-local Type, Version = "Icon", 21
+local Type, Version = "Icon", 22
 local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
 if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
 
--- AceGUI-3.0/widgets/AceGUIWidget-Label.lua
+++ AceGUI-3.0/widgets/AceGUIWidget-Label.lua
@@ -2,7 +2,7 @@
 Label Widget
 Displays text and optionally an icon.
 -------------------------------------------------------------------------------]]
-local Type, Version = "Label", 28
+local Type, Version = "Label", 29
 local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
 if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
 
